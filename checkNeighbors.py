476e1f25948a267981c6e2d44c18fafe5e85f860
bruno <bruno@bruno-N56VZ.(none)>
2015-04-14 11:35:04 +0100

    query match added

--- checkNeighbors.py
+++ checkNeighbors.py
@@ -151,7 +151,7 @@ def GetNeighbours(arrayOfGenes, arrayOfSequences, targetGene, queryName, querySe
 
 		#ToWrite.append('>'+queryName+'\n'+querySequence+'\n') # INSERT QUERY ON THE RESULTS
 		
-		for x in range(targetIndex+1, targetIndex+numberDownstream+1):
+		for x in range(targetIndex, targetIndex+numberDownstream+1):
 			try:
 				ToWrite.append('>'+arrayOfGenes[x]+"\n"+arrayOfSequences[x]+'\n')
 			except IndexError:
